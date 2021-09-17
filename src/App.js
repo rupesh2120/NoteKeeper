088342b0fd1c6c5e94fae64d502dd681134134d6
Rupesh <rupeshsharma2196@gmail.com>
2021-09-17 09:54:15 +0530

    Pushing frontend app

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import { Home } from "./components/Home";
+import { About } from "./components/About";
+import NoteState from "./context/notes/NoteState";
+import { Alert } from "./components/Alert";
+import { Login } from "./components/Login";
+import { Signup } from "./components/Signup";
+
+function App() {
+	return (
+		<div>
+			<NoteState>
+				<Router>
+					<Navbar />
+					<Alert message="Notekeeper" />
+					<div className="container">
+						<Switch>
+							<Route exact path="/">
+								<Home />
+							</Route>
+							<Route exact path="/about">
+								<About />
+							</Route>
+							<Route exact path="/login">
+								<Login />
+							</Route>
+							<Route exact path="/signup">
+								<Signup />
+							</Route>
+						</Switch>
+					</div>
+				</Router>
+			</NoteState>
+		</div>
+	);
+}
+
+export default App;
